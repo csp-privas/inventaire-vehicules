e231248d974f2356886c2d32b31e1bf755ccfe81
CPS-PRI <antoinemerchat@gmail.com>
2026-07-27 09:20:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "inventaire-vehicules-v24";
+const CACHE_NAME = "inventaire-vehicules-v25";
 const APP_SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
